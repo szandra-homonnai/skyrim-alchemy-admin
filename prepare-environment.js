f7f91602a5277c0bf5ff8d19c398b3f1b392e3cf
Szandra Homonnai <szandra.homonnai@gmail.com>
2023-05-24 17:05:12 +0200

    feat: add basic firebase configs

---
+++ prepare-environment.js
@@ -0,0 +1,29 @@
+const dotenv = require('dotenv');
+const fs = require('fs');
+
+const targetPath = './src/environments/environment.ts';
+const envPath = './src/environments/.env';
+
+dotenv.config({ path: envPath });
+
+const environmentFile = `export const environment = {
+  firebase: {
+    projectId: '${process.env.FIREBASE_PROJECT_ID}',
+    appId: '${process.env.FIREBASE_APP_ID}',
+    databaseURL: '${process.env.FIREBASE_DATABASE_URL}',
+    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
+    locationId: '${process.env.FIREBASE_LOCATION_ID}',
+    apiKey: '${process.env.FIREBASE_API_KEY}',
+    authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
+    messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}'
+  }
+};
+`;
+
+fs.writeFile(targetPath, environmentFile, 'utf8', (error) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(`${targetPath} has been successfully updated.`);
+  }
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,7 @@
 import { NgModule } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -6,6 +9,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from '@app/app-routing.module';
 import { AppComponent } from '@app/app.component';
+import { environment } from '@environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,7 +21,10 @@ import { AppComponent } from '@app/app.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
